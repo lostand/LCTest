c9a70d0fe53e4d130c1c4ac57b47dc2d9ba95122
lostand <libin.cheng@yahoo.com>
2021-08-22 20:28:32 +0800

    0.0.5 提交bug

--- LCTest7/LCTest7/Version2ViewController.swift
+++ LCTest7/LCTest7/Version2ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  Version1ViewController.swift
+//  Version2ViewController.swift
 //  LCTest7
 //
 //  Created by chenglibin on 2021/8/22.
@@ -7,24 +7,18 @@
 
 import UIKit
 
-class Version1ViewController: UIViewController {
+class Version2ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         let btn = UIButton(frame: CGRect(x: 100, y: 200, width: 300, height: 200))
-        btn.setTitle("0.0.4版本", for: .normal)
+        btn.setTitle("0.0.5版本", for: .normal)
         btn.backgroundColor = .green
-        btn.addTarget(self, action: #selector(btnAction), for: .touchUpInside)
         self.view.addSubview(btn)
 
         // Do any additional setup after loading the view.
     }
-    
-    @objc func btnAction(){
-        self.present(Version2ViewController(), animated: true, completion: nil)
-    }
-
     
 
     /*
