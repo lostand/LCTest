66d0465b1afcb7f4aab6f99547093f8b67633332
lostand <libin.cheng@yahoo.com>
2021-08-22 20:57:26 +0800

    0.0.6改变

--- LCTest7/LCTest7/AppDelegate.swift
+++ LCTest7/LCTest7/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("version0.0.5")
+        print("version0.0.6")
         // Override point for customization after application launch.
         return true
     }
