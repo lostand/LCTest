4fe505d7384377542b5b31a8d7ba693eae1d1a06
lostand <libin.cheng@yahoo.com>
2021-08-22 19:54:16 +0800

    初始工程

--- LCTest7/LCTest7/ViewController.swift
+++ LCTest7/LCTest7/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let btn = UIButton(frame: CGRect(x: 100, y: 200, width: 300, height: 200))
+        btn.setTitle("初始工程", for: .normal)
+        btn.backgroundColor = .green
+        self.view.addSubview(btn)
         // Do any additional setup after loading the view.
     }
 
